ad298884c76fceb94ea9613b0e7e9e8192fec273
Luis Reisewitz <reisewitz@me.com>
2016-05-15 16:19:17 +0200

    switch to front camera and detect faces

--- DisplayLiveSamples/SessionHandler.swift
+++ DisplayLiveSamples/SessionHandler.swift
@@ -8,20 +8,30 @@
 
 import AVFoundation
 
-class SessionHandler : NSObject, AVCaptureVideoDataOutputSampleBufferDelegate {
+class SessionHandler : NSObject, AVCaptureVideoDataOutputSampleBufferDelegate, AVCaptureMetadataOutputObjectsDelegate {
     var session = AVCaptureSession()
     let layer = AVSampleBufferDisplayLayer()
     let sampleQueue = dispatch_queue_create("com.zweigraf.DisplayLiveSamples.sampleQueue", DISPATCH_QUEUE_SERIAL)
+    let faceQueue = dispatch_queue_create("com.zweigraf.DisplayLiveSamples.faceQueue", DISPATCH_QUEUE_SERIAL)
     
     override init() {
         super.init()
     }
     
     func openSession() {
+        let device = AVCaptureDevice.devicesWithMediaType(AVMediaTypeVideo)
+            .map { $0 as! AVCaptureDevice }
+            .filter { $0.position == .Front}
+            .first!
+        
+        let input = try! AVCaptureDeviceInput(device: device)
+        
         let output = AVCaptureVideoDataOutput()
         output.setSampleBufferDelegate(self, queue: sampleQueue)
-        let device = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
-        let input = try! AVCaptureDeviceInput(device: device)
+    
+        let metaOutput = AVCaptureMetadataOutput()
+        metaOutput.setMetadataObjectsDelegate(self, queue: faceQueue)
+        
         
         session.beginConfiguration()
         
@@ -31,11 +41,16 @@ class SessionHandler : NSObject, AVCaptureVideoDataOutputSampleBufferDelegate {
         if session.canAddOutput(output) {
             session.addOutput(output)
         }
-        
-        output.connectionWithMediaType(AVMediaTypeVideo).videoOrientation = .Portrait
+        if session.canAddOutput(metaOutput) {
+            session.addOutput(metaOutput)
+        }
         
         session.commitConfiguration()
        
+        // availableMetadataObjectTypes change when output is added to session.
+        // before it is added, availableMetadataObjectTypes is empty
+        metaOutput.metadataObjectTypes = [AVMetadataObjectTypeFace]
+        
         session.startRunning()
     }
     
@@ -43,5 +58,11 @@ class SessionHandler : NSObject, AVCaptureVideoDataOutputSampleBufferDelegate {
     // MARK: AVCaptureVideoDataOutputSampleBufferDelegate
     func captureOutput(captureOutput: AVCaptureOutput!, didOutputSampleBuffer sampleBuffer: CMSampleBuffer!, fromConnection connection: AVCaptureConnection!) {
         layer.enqueueSampleBuffer(sampleBuffer)
+    }
+    
+    // MARK: AVCaptureMetadataOutputObjectsDelegate
+    
+    func captureOutput(captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [AnyObject]!, fromConnection connection: AVCaptureConnection!) {
+        print(metadataObjects)
     }
 }

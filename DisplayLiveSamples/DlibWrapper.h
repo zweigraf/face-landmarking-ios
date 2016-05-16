79009c75d016dbc385e3721d5b3b7a757509e197
Luis Reisewitz <reisewitz@me.com>
2016-05-16 18:50:57 +0200

    add DliBWrapper file which will hold cpp code and add bridging header so that the objective-cpp code can be called from swift

---
+++ DisplayLiveSamples/DlibWrapper.h
@@ -0,0 +1,13 @@
+//
+//  DlibWrapper.h
+//  DisplayLiveSamples
+//
+//  Created by Luis Reisewitz on 16.05.16.
+//  Copyright © 2016 ZweiGraf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DlibWrapper : NSObject
+
+@end
---
+++ DisplayLiveSamples/DlibWrapper.mm
@@ -0,0 +1,13 @@
+//
+//  DlibWrapper.m
+//  DisplayLiveSamples
+//
+//  Created by Luis Reisewitz on 16.05.16.
+//  Copyright © 2016 ZweiGraf. All rights reserved.
+//
+
+#import "DlibWrapper.h"
+
+@implementation DlibWrapper
+
+@end
